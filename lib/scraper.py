f0aca5257e049883a3d36cea3438d84e3df7bec1
Shane <haroboy876@gmail.com>
2015-11-10 15:14:59 -0800

    comment pass

--- lib/scraper.py
+++ lib/scraper.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# TODO : fix analytics module
-
 from config import *
 from lib.libs import *
 from bs4 import BeautifulSoup # For HTML parsing
@@ -13,89 +11,91 @@ import pandas as pd # For converting results to a dataframe and bar chart plots
 import requests
 import json
 
-def rstyle_search(advertiser):
-    url = "https://api.rewardstyle.com/v1/search?oauth_token={}&advertisers[]={}".format(access_token, advertiser)
+def rstyle_search(advertiser): # do a search for a given advertiser
+    url = "https://api.rewardstyle.com/v1/search?oauth_token={}&advertisers[]={}".format(
+        access_token, advertiser)
     resp = requests.get(url)
-    data = json.loads(resp.content)
-    product_id = data["products"][0]["product_id"]
+    data = json.loads(resp.content) # if there is a result, grab the json
+    product_id = data["products"][0]["product_id"] # snag the product id of the top result
     return product_id
 
 
-def rstyle_link(advertiser):
+def rstyle_link(advertiser): # gets a shortlink
     try:
         rstyle_search(advertiser)
     except Exception as error:
         return error
-    url = "https://api.rewardstyle.com/v1/get_link?oauth_token={}&product={}".format(access_token, rstyle_search(advertiser))
+    url = "https://api.rewardstyle.com/v1/get_link?oauth_token={}&product={}".format(
+        access_token, rstyle_search(advertiser))
     resp = requests.get(url)
-    data = json.loads(resp.content)
-    link = data["link"]
+    data = json.loads(resp.content) # this is the json for the shortlink request
+    link = data["link"] # snag the shortlink
     print "short link:", link
     return link
 
 
-def text_cleaner(stripped_advertiser, website):
+def text_cleaner(stripped_advertiser, website): # clean up html and read words
     soup_obj = BeautifulSoup(website, "html.parser")
-    for script in soup_obj(["script", "style"]):
+    for script in soup_obj(["script", "style"]): # get rid of that nasty javascript
         script.extract() # Remove these two elements from the BS4 object
-    text = soup_obj.get_text()
+    text = soup_obj.get_text() # we convert text in place several times coming up
     with open('sites/{}.html'.format(stripped_advertiser.lower()), 'w') as f:
-        f.write(soup_obj.encode("utf-8"))
+        f.write(soup_obj.encode("utf-8")) #write html out to file
     lines = (line.strip() for line in text.splitlines()) # break into lines
     chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-    def chunk_space(chunk):
+    def chunk_space(chunk): # individual utf-8 encoded word chunks
         chunk_out = chunk + " "
         return chunk_out
     text = "".join(chunk_space(chunk) for chunk in chunks if chunk).encode("utf-8")
     try:
         text = text.decode("unicode_escape").encode("ascii", "ignore")
     except Exception as error:
-        return
-    text = re.sub("[^a-zA-Z.+3]"," ", text)
-    text = text.lower().split()
+        return # just in case there are some weird characters here, don't kill app
+    text = re.sub("[^a-zA-Z.+3]"," ", text) # only regular words, no numbers
+    text = text.lower().split() # lower case so the dict can be properly populated
     stop_words = set(stopwords.words("english"))
-    text = [w for w in text if not w in stop_words]
-    fashion_dict["Wrinkle"] = 1
-    text = list(set(text))
+    text = [w for w in text if not w in stop_words] # gets rid of words like "and"
+    text = list(set(text)) # no repeats!
     for word in text:
         if word not in fashion_dict:
-            fashion_dict[word] = 1
+            fashion_dict[word] = 1 # populate fashion dict
         else:
-            fashion_dict[word] += 1
+            fashion_dict[word] += 1 # increment entry
     return text
 
 
 def scrape():
-    successful_scrapes = 0
-    for i in range(len(advertisers)):
+    successful_scrapes = 0 # keep track of scrapes that went through
+    for i in range(len(advertisers)): # per advertiser
         raw_advertiser = advertisers[i].encode("ascii", "xmlcharrefreplace").lower()
         print "advertiser raw:", raw_advertiser
         advertiser = raw_advertiser
         if "and" in advertiser:
-            advertiser = advertiser.split("and")[0].strip(" ")
+            advertiser = advertiser.split("and")[0].strip(" ") # separate multiple
+            # advertisers and search for only the first listed
         if ".com" in advertiser:
-            advertiser = advertiser.replace(".com", "")
-        stripped_advertiser = advertiser.replace(" ", "_")
-        advertiser = urllib2.quote(advertiser)
+            advertiser = advertiser.replace(".com", "") # could prevent search result
+        stripped_advertiser = advertiser.replace(" ", "_") # get rid of spaces for file name
+        advertiser = urllib2.quote(advertiser) # url safe version
         print "advertiser (url safe):", advertiser
         print "Working on advertiser {} out of {} ({})".format(i + 1,
             len(advertisers), raw_advertiser)
         try:
-            short_url = rstyle_link(advertiser)
+            short_url = rstyle_link(advertiser) # get shortlink
             short_resp = requests.get(short_url)
-            short_data = short_resp.content.encode("utf-8")
+            short_data = short_resp.content.encode("utf-8") # generate unicode data
         except Exception as error:
             print "Error while scraping. No results found for advertiser"
-            continue
+            continue # nothing else to do without the result
         try:
             follow_on_url = re.search('<!-- (.*) -->', short_data).group(1)
             print "follow on url:", follow_on_url
             follow_on_resp = requests.get(follow_on_url)
             print "final follow on url:", follow_on_resp.url
-            follow_on_data = str(follow_on_resp.content)
+            follow_on_data = str(follow_on_resp.content) # final html
             text_cleaner(stripped_advertiser, follow_on_data.decode("utf-8"))
             print "advertiser {} scraped".format(i + 1)
-            successful_scrapes += 1
+            successful_scrapes += 1 # great work - scrape successful
         except Exception as error:
             print error
 
@@ -108,13 +108,14 @@ def scrape():
         if intermediate_total_skills[key] > 0:
             overall_total_skills[key] = intermediate_total_skills[key]
 
-    final_frame = pd.DataFrame(overall_total_skills.items(), columns = ["Term", "NumPostings"])
-    pd.set_option('display.height', 500000)
+    final_frame = pd.DataFrame(overall_total_skills.items(), columns = ["Term",
+        "NumPostings"])
+    pd.set_option('display.height', 500000) # this is just an arbitrary maximum
     pd.set_option('display.max_rows', 500000)
     final_frame.NumPostings = ((final_frame.NumPostings)*100)/successful_scrapes
     final_frame.sort_values(by= "NumPostings", ascending = False, inplace = True)
     import time
     with open('sites/word_freq_{}.txt'.format(int(time.time())), 'w') as f:
-        f.write(str(final_frame))
+        f.write(str(final_frame)) # why not write out the analytics to file?
 
     return final_frame # End of the function
